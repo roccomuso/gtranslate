270f58400285b6a78db1b1aa01cd9d41f235b457
rocco <rocco@hackerstribe.com>
2017-07-16 19:29:00 +0200

    main logic

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,35 @@
 #!/usr/bin/env node
 'use strict'
+const boxen = require('boxen')
+var argv = require('../lib/argv')
+var config = require('../lib/config')
 
-// TODO
+var API_KEY = config.getKey()
+
+if (!API_KEY && !argv.register) {
+  exit(boxen('Missing API_KEY for google translate.\nUse the --register option', {align: 'center', borderColor: 'red', padding: 1, margin: 1}), 1)
+}
+
+if (argv.register) {
+  config.setKey(argv.register)
+  exit(boxen('\u2714 KEY stored!', {align: 'center', borderColor: 'green', padding: 1, margin: 1}), 0)
+}
+
+if (argv.st) {
+  config.setTargetLang(st)
+  exit(boxen('\u2714 Target lang set!', {align: 'center', borderColor: 'green', padding: 1, margin: 1}), 0)
+}
+
+var googleTranslate = require('google-translate')(API_KEY);
+
+googleTranslate.translate(argv.text, argv.source, argv.target || config.getTargetLang(), function (err, translation){
+  var translatedText = translation.translatedText
+  var sourceLanguage = translation.detectedSourceLanguage || argv.source
+  var targetLanguage = translation.targetLanguage
+  console.log(boxen(`${sourceLanguage} \u2192 ${targetLanguage}\n\n${translatedText} `, {align: 'center', borderColor: 'green', padding: 1, margin: 1}))
+})
+
+function exit(msg, code) {
+  console.log(msg)
+  process.exit(code || 0)
+}
