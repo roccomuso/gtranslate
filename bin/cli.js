2a9606f73dc101a5599e796a8b9dbc0681ce1f8b
rocco <rocco@hackerstribe.com>
2017-07-16 19:31:15 +0200

    bug fix

--- bin/cli.js
+++ bin/cli.js
@@ -16,20 +16,21 @@ if (argv.register) {
 }
 
 if (argv.st) {
-  config.setTargetLang(st)
+  config.setTargetLang(argv.st)
   exit(boxen('\u2714 Target lang set!', {align: 'center', borderColor: 'green', padding: 1, margin: 1}), 0)
 }
 
-var googleTranslate = require('google-translate')(API_KEY);
+var googleTranslate = require('google-translate')(API_KEY)
 
-googleTranslate.translate(argv.text, argv.source, argv.target || config.getTargetLang(), function (err, translation){
+googleTranslate.translate(argv.text, argv.source, argv.target || config.getTargetLang(), function (err, translation) {
+  if (err) return exit(err.message, 1)
   var translatedText = translation.translatedText
   var sourceLanguage = translation.detectedSourceLanguage || argv.source
   var targetLanguage = translation.targetLanguage
   console.log(boxen(`${sourceLanguage} \u2192 ${targetLanguage}\n\n${translatedText} `, {align: 'center', borderColor: 'green', padding: 1, margin: 1}))
 })
 
-function exit(msg, code) {
+function exit (msg, code) {
   console.log(msg)
   process.exit(code || 0)
 }
