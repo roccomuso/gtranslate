cfd7e58f9bae487d05ec42dc2ebd799215b30186
David Dikman <ddikman@gmail.com>
2018-10-31 08:38:10 +0000

    Added in the absolute basic tests

---
+++ test/test-runner.js
@@ -0,0 +1,18 @@
+var exec = require('child_process').execSync
+
+// Helper function to execute the cli in tests and capture the output
+module.exports = function (parameters) {
+  try {
+    var output = exec(`./bin/cli.js ${parameters}`).toString()
+    return {
+      output: output,
+      code: 0
+    }
+  } catch (error) {
+    return {
+      output: error.stdout.toString(),
+      error: error.stderr.toString(),
+      code: error.status
+    }
+  }
+}
