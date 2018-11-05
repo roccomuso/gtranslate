6ee4493cd0393f0fd201c88d5b4d17a4a04ff35a
David Dikman <ddikman@gmail.com>
2018-11-05 21:37:22 +0000

    Added done for default language test

--- test/default-language-option-test.js
+++ test/default-language-option-test.js
@@ -4,8 +4,9 @@ var runWithParameters = require('./test-runner')
 chai.should()
 
 describe('default language option', () => {
-  it('can set default language', () => {
+  it('can set default language', (done) => {
     var result = runWithParameters('--set-target en')
     result.output.should.contain('Target lang set!')
+    done()
   })
 })
