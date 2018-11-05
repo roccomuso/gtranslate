f99a9cb49820ab51bcee8ee518412f256a783a04
David Dikman <ddikman@gmail.com>
2018-11-05 10:01:57 +0000

    Just trying to add in a done

--- test/boxed-test.js
+++ test/boxed-test.js
@@ -5,11 +5,12 @@ const boxed = require('./boxed-tester')
 chai.should()
 
 describe('boxed output', function () {
-  it('should return boxed output if no brief option is given', function () {
+  it('should return boxed output if no brief option is given', function (done) {
     var result = runWithParameters('-s en -t no sandwich')
     var output = boxed(result.output)
     output.translation.should.equal('smørbrød')
     output.sourceLanguage.should.equal('en')
     output.targetLanguage.should.equal('no')
+    done()
   })
 })
