6ab859437f09d28520a11a831b97d018fa4e2ec0
David Dikman <ddikman@gmail.com>
2018-11-05 11:31:59 +0000

    Another test

--- test/default-language-option-test.js
+++ test/default-language-option-test.js
@@ -6,6 +6,7 @@ chai.should()
 describe('default language option', function () {
   it('can set default language', function () {
     var result = runWithParameters('--set-target en')
+    console.log(`got output from run: ${JSON.stringify(result)}`)
     result.output.should.contain('Target lang set!')
   })
 })
