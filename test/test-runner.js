6c2b81c17cd02812f58f1c9c85bf2ed7538373fc
David Dikman <ddikman@gmail.com>
2018-11-05 08:57:21 +0000

    Test output

--- test/test-runner.js
+++ test/test-runner.js
@@ -4,14 +4,15 @@ var exec = require('child_process').execSync
 module.exports = function (parameters) {
   try {
     var output = exec(`./bin/cli.js ${parameters}`).toString()
+    console.log("got output: \n" + output)
     return {
       output: output,
       code: 0
     }
   } catch (error) {
     return {
-      output: error.stdout.toString(),
-      error: error.stderr.toString(),
+      output: (error.stdout || '').toString(),
+      error: (error.stderr || '').toString(),
       code: error.status
     }
   }
