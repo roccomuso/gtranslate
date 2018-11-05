138e802155f73daacc975c971c50ed31a47c359d
David Dikman <ddikman@gmail.com>
2018-11-05 09:00:21 +0000

    Tried bumping node version again

--- test/test-runner.js
+++ test/test-runner.js
@@ -4,15 +4,14 @@ var exec = require('child_process').execSync
 module.exports = function (parameters) {
   try {
     var output = exec(`./bin/cli.js ${parameters}`).toString()
-    console.log("got output: \n" + output)
     return {
       output: output,
       code: 0
     }
   } catch (error) {
     return {
-      output: (error.stdout || '').toString(),
-      error: (error.stderr || '').toString(),
+      output: error.stdout.toString(),
+      error: error.stderr.toString(),
       code: error.status
     }
   }
