d38cea1bc7811fa189816ddf3b513e5682ca3f64
David Dikman <ddikman@gmail.com>
2018-11-05 08:28:27 +0000

    Added in extra logging

--- test/test-runner.js
+++ test/test-runner.js
@@ -3,12 +3,15 @@ var exec = require('child_process').execSync
 // Helper function to execute the cli in tests and capture the output
 module.exports = function (parameters) {
   try {
+    console.log(`running [./bin/cli.js ${parameters}]`)
     var output = exec(`./bin/cli.js ${parameters}`, { shell: false }).toString()
+    console.log('output received')
     return {
       output: output,
       code: 0
     }
   } catch (error) {
+    console.log('error caught')
     return {
       output: error.stdout.toString(),
       error: error.stderr.toString(),
