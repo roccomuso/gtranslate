5dd2a907b19f95453af6300cd87ee53d9b8a9331
David Dikman <ddikman@gmail.com>
2018-11-06 08:07:40 +0000

    Added logging

--- test/test-runner.js
+++ test/test-runner.js
@@ -2,7 +2,9 @@ var exec = require('child_process').exec
 
 // Helper function to execute the cli in tests and capture the output
 module.exports = function (parameters, onComplete) {
+  console.log('executing')
   exec(`./bin/cli.js ${parameters}`, (error, stdout, stderr) => {
+    console.log('handling result')
     if (error) {
       console.log('resulted in error')
       onComplete({ code: error.code, output: stdout, error: stderr })
