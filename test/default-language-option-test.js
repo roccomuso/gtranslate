46393397a48efc80a6e8240050fe50de44d25bc8
David Dikman <ddikman@gmail.com>
2018-11-06 08:01:32 +0000

    attemted to add in login to zoom in on the issue

--- test/default-language-option-test.js
+++ test/default-language-option-test.js
@@ -5,8 +5,11 @@ chai.should()
 
 describe('default language option', () => {
   it('can set default language', (done) => {
+    console.log('executing')
     runWithParameters('--set-target en', (result) => {
+      console.log('response recieved')
       result.output.should.contain('Target lang set!')
+      console.log('calling done')
       done()
       console.log('default language option test is done.')
     })
--- test/test-runner.js
+++ test/test-runner.js
@@ -4,8 +4,10 @@ var exec = require('child_process').exec
 module.exports = function (parameters, onComplete) {
   exec(`./bin/cli.js ${parameters}`, (error, stdout, stderr) => {
     if (error) {
+      console.log('resulted in error')
       onComplete({ code: error.code, output: stdout, error: stderr })
     } else {
+      console.log('resulted successfully')
       onComplete({ code: 0, output: stdout, error: null })
     }
   })
